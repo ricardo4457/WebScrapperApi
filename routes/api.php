dfd200270d57652383e6fbc57079dc2f77f7a187
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-07-10 19:11:14 +0100

    feat : create endpoint to acess WebScrapper

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,16 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ScrapeController;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:sanctum');
+
+
+Route::post('/book-scraper/callback', [ScrapeController::class, 'callback'])
+    ->middleware('node.apikey')
+    ->name('book-scraper.callback');
+
+Route::post('/book-scraper/run', [ScrapeController::class, 'runScrape'])
+    ->middleware(['auth:sanctum', 'throttle:10,1']);
