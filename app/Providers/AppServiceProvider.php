c63b29208c3d86648d613068cca7c643f4c0603a
ricardo4457 <ricardo.a.vieira123@gmail.com>
2026-08-01 02:20:33 +0100

    feat : put rate limiter on the correct main file without creating a new middleware

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,24 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Cache\RateLimiting\Limit;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
     public function register(): void
     {
         //
     }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        RateLimiter::for('scrape-run', function ($request) {
+            return Limit::perMinute(5)->by($request->ip());
+        });
+
+        RateLimiter::for('scrape-run-heavy', function ($request) {
+            return Limit::perMinute(1)->by($request->ip());
+        });
     }
 }
